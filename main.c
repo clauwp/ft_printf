9387aee334aabcb30fa27017ad32456ccf80a308
Cheryl Lau <clau@u81z08s03.42kl.edu.my>
2021-11-03 18:30:47 +0800

    main.c

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main(void)
 {
     setbuf(stdout, NULL);  // this will force printf to print immediately
     printf("ft_printf:\n");
-    int len = ft_printf(" %05d ", -11);
+    int len = ft_printf("[%-2c]\n",0);
     printf("\n");
     printf("printf:\n");
-    int len2 = printf(" %05d ", -11);
+    int len2 = printf("[%-2c]\n",0);
     printf("\n");
     printf("len1 = %d\n", len);
     printf("len2 = %d\n", len2);
